97ea3bc82ccfe5b5c539351ed9d4d648b65d7ff3
Adarsh <adarsh.bhadauria@uolo.com>
2022-03-10 22:47:28 +0530

    Moves piece drag & drop logic to board component

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useEffect } from "react";
 
 import Square from "./Square";
 import { ALPHABETS, PIECE_DEFAULT_POSITION_LIST } from "../../utils/constants";
@@ -17,6 +18,58 @@ const BoardContainer = styled.div`
 
 export default function Board({ isWhitePlayer = true }) {
   let boardProperties = [];
+
+  useEffect(() => {
+    // select all pieces
+    const pieces = document.querySelectorAll(".piece");
+
+    // add dragStart eventListener to all
+    pieces.forEach((piece) => piece.addEventListener("dragstart", dragStart));
+
+    function dragStart(e) {
+      e.dataTransfer.setData("text/plain", e.target.id);
+      setTimeout(() => {
+        e.target.classList.add("hide");
+      }, 0);
+    }
+
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+      square.addEventListener("dragenter", dragEnter);
+      square.addEventListener("dragover", dragOver);
+      square.addEventListener("dragleave", dragLeave);
+      square.addEventListener("drop", drop);
+    });
+
+    function dragEnter(e) {
+      e.preventDefault();
+      e.target.classList.add("drag-over");
+    }
+
+    function dragOver(e) {
+      e.preventDefault();
+      e.target.classList.add("drag-over");
+    }
+
+    function dragLeave(e) {
+      e.target.classList.remove("drag-over");
+    }
+
+    function drop(e) {
+      e.target.classList.remove("drag-over");
+
+      // get the draggable element
+      const id = e.dataTransfer.getData("text/plain");
+      const draggable = document.getElementById(id);
+
+      // add it to the drop target
+      e.target.appendChild(draggable);
+
+      // display the draggable element
+      draggable.classList.remove("hide");
+    }
+  });
 
   // Generate board properties & arrange pieces to default location
   for (let i = 8; i > 0; i -= 1) {
