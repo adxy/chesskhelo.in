4d47b339e8303ab313467a6f518e6ef791547022
Adarsh <theadxy@gmail.com>
2022-09-27 20:18:00 +0530

    fix: userState was not defined in /play

--- pages/play.jsx
+++ pages/play.jsx
@@ -1,10 +1,13 @@
 import dynamic from "next/dynamic";
 
+import { useUserState } from "../store/user";
+
 const DynamicBoard = dynamic(() => import("../components/Board/Board"), {
   ssr: false,
 });
 
 export default function Play() {
+  const [userState, _] = useUserState();
   return (
     <>
       {userState.loggedIn && (
